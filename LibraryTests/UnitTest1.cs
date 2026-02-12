2a3f4f941fc1d30cf2cca8a15f691b790ae505e2
oljh <oljh@itu.dk>
2026-02-12 15:15:48 +0100

    Add test project

---
+++ LibraryTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(Library.Class1.PrintingString, Is.EqualTo("Hallelujah"));
+    }
+}
