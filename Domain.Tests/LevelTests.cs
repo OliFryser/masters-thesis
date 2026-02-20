e2a7ff0fc5ad51455665c93844721d710fd48cbd
oljh <oljh@itu.dk>
2026-02-20 14:21:15 +0100

    Added Domain.Tests project

---
+++ Domain.Tests/LevelTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Level_Has_UniqueCells()
+    {
+        // Arrange
+        List<Cell> cells = new List<Cell>
+        {
+            new Cell(new Vector(0,0), new HashSet<TileOption>()),
+            new Cell(new Vector(0,0), new HashSet<TileOption>()),
+        };
+
+        // Act
+        var level = new Level(cells);
+
+        // Assert
+        Assert.That(level.Cells, Has.Count.EqualTo(1));
+    }
+    
+    [Test]
+    public void Level_Has_CellLookupByPosition()
+    {
+        // Arrange
+        Cell cell00 = new Cell(new Vector(0, 0), new HashSet<TileOption>());
+        Cell cell10 = new Cell(new Vector(1, 0), new HashSet<TileOption>()); 
+        List<Cell> cells = new List<Cell>
+        {
+            cell00,
+            cell10,
+        };
+
+        // Act
+        var level = new Level(cells);
+
+        // Assert
+        Assert.That(level.Cells, Has.Count.EqualTo(2));
+        Assert.That(level.CellLookup[new Vector(0, 0)], Is.EqualTo(cell00));
+    }
+}
