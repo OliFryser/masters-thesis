eb408236eb6e9f9963342da43fd144a30aa5df46
William Skou Heidemann <Williamskou2227@gmail.com>
2026-02-22 07:49:01 +0100

    Cell tests

--- Pokémon/Class1.cs
+++ Pokémon/Class1.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Domain;
 
 namespace Pokémon
 {
     public class Class1
     {
+        public void Run()
+        {
+            TileType water = new TileType
+            {
+                Type = "Water"
+            };
+
+            Vector[] positions = new Vector[1];
+
+            HashSet<TileOption> options = new HashSet<TileOption>
+            {
+                new TileOption
+                {
+                    TileType = water,
+                    Weight = 1
+                }
+            };
+
+            ReadOnlyDictionary<TileType, TileRules> rules = new ReadOnlyDictionary<TileType, TileRules>(
+                new Dictionary<TileType, TileRules>()
+                {
+                    {
+                        water, new TileRules()
+                    }
+                });
+
+            Level level = WFC.WaveFunctionCollapse.Generate(positions, options, rules);
+        }
     }
 }
--- WFC.Tests/CellExtensionTests.cs
+++ WFC.Tests/CellExtensionTests.cs
@@ -5,30 +5,28 @@ namespace WFC.Tests;
 
 public class Tests
 {
+    private Cell _cellWithZeroOptions;
+    private Cell _cellWithOneOption;
+    private Cell _cellWithManyOptions;
+
     [SetUp]
     public void Setup()
     {
+        _cellWithZeroOptions = GetCellWithZeroOptions();
+        _cellWithOneOption = GetCellWithOneOption();
+        _cellWithManyOptions = GetCellWithManyOptions();
     }
-
-    [Test]
-    public void Cell_Without_Options_Is_Not_Collapsable()
+    
+    private Cell GetCellWithZeroOptions()
     {
-        // Arrange
         Vector position = new(0, 0);
         HashSet<TileOption> options = [];
         Cell cell = new(position, options);
-
-        // Act
-        bool isCollapsable = cell.IsCollapsable();
-        
-        // Assert
-        Assert.That(isCollapsable, Is.False);
+        return cell;
     }
-
-    [Test]
-    public void Cell_With_One_Option_Is_Collapsable()
+    
+    private Cell GetCellWithOneOption()
     {
-        // Arrange
         Vector position = new(0, 0);
         
         TileOption option = new()
@@ -42,18 +40,12 @@ public class Tests
         HashSet<TileOption> options = [option];
         
         Cell cell = new(position, options);
-
-        // Act
-        bool isCollapsable = cell.IsCollapsable();
         
-        // Assert
-        Assert.That(isCollapsable, Is.True);
+        return cell;
     }
     
-    [Test]
-    public void Cell_With_Many_Options_Is_Collapsable()
+    private Cell GetCellWithManyOptions()
     {
-        // Arrange
         Vector position = new(0, 0);
         
         TileOption option = new()
@@ -72,14 +64,106 @@ public class Tests
             }
         };
 
-        HashSet<TileOption> options = [option, option];
+        HashSet<TileOption> options = [option, option2];
         
         Cell cell = new(position, options);
 
+        return cell;
+    }
+
+    [Test]
+    public void Cell_Without_Options_Is_Not_Collapsable()
+    {
+        // Arrange
+
         // Act
-        bool isCollapsable = cell.IsCollapsable();
+        bool isCollapsable = _cellWithZeroOptions.IsCollapsable();
+        
+        // Assert
+        Assert.That(isCollapsable, Is.False);
+    }
+
+    [Test]
+    public void Cell_With_One_Option_Is_Collapsable()
+    {
+        // Arrange
+        
+        // Act
+        bool isCollapsable = _cellWithOneOption.IsCollapsable();
         
         // Assert
         Assert.That(isCollapsable, Is.True);
+    }
+    
+    [Test]
+    public void Cell_With_Many_Options_Is_Collapsable()
+    {
+        // Arrange
+
+        // Act
+        bool isCollapsable = _cellWithManyOptions.IsCollapsable();
+        
+        // Assert
+        Assert.That(isCollapsable, Is.True);
+    }
+    
+    [Test]
+    public void Collapse_Cell_Leaves_Only_One_Option()
+    {
+        // Arrange
+        
+        // Act
+        Cell collapsedCell = _cellWithManyOptions.CollapseCell();
+        
+        // Assert
+        Assert.That(collapsedCell.IsCollapsed(), Is.True);
+    }
+
+    [Test]
+    public void Collapsing_A_Cell_With_No_Options_Throws()
+    {
+        // Arrange
+        Func<Cell> collapseAction = () => _cellWithZeroOptions.CollapseCell();
+        
+        // Act
+        
+        // Assert
+        Assert.That(collapseAction, Throws.TypeOf<InvalidOperationException>());
+    }
+
+    [Test]
+    public void Cell_With_Zero_Options_Is_Not_Collapsed()
+    {
+        // Act
+        bool isCollapsed = _cellWithZeroOptions.IsCollapsed();
+        
+        // Assert
+        Assert.That(isCollapsed, Is.False);
+    }
+
+    [Test]
+    public void Cell_With_One_Option_Is_Collapsed()
+    {
+        // Act
+        bool isCollapsed = _cellWithOneOption.IsCollapsed();
+        
+        // Assert
+        Assert.That(isCollapsed, Is.True);
+    }
+
+    [Test]
+    public void Cell_With_Many_Options_Is_Not_Collapsed()
+    {
+        // Act
+        bool isCollapsed = _cellWithManyOptions.IsCollapsed();
+        
+        // Arrange
+        Assert.That(isCollapsed, Is.False);
+    }
+
+    [Test]
+    public void Option_With_High_Weight_Is_Chosen_Most_Often()
+    {
+        
     }
 }
