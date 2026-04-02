18e287b3f4f1993beb6110cf34f0219ab9479ba9
William Skou Heidemann <Williamskou2227@gmail.com>
2026-04-02 08:34:17 +0200

    update dlls

--- DotNet/CLI/Program.cs
+++ DotNet/CLI/Program.cs
@@ -4,11 +4,11 @@ using MapElites.Args;
 using MapElites.Models;
 using Pokémon;
 
-string resourceDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}/../../../Resources/";
-string tilemapPath = $"{resourceDirectory}Tilemaps/PalletTown.png";
+string resourceDirectory = $"{AppDomain.CurrentDomain.BaseDirectory}/../../../Resources";
+string tilemapPath = $"{resourceDirectory}/Tilemaps/PalletTown.png";
 
-MapElitesArgs mapElitesArgs = new MapElitesArgs(100, 1000, Console.WriteLine);
-IndividualHandler individualHandler = new IndividualHandler(tilemapPath);
+MapElitesArgs mapElitesArgs = new(100, 1000, Console.WriteLine);
+IndividualHandler individualHandler = new(tilemapPath);
 
 Stopwatch stopwatch = Stopwatch.StartNew();
 Archive<Key, Entry, Individual, Behavior> archive = MapElites.MapElites.Run(individualHandler, mapElitesArgs);
