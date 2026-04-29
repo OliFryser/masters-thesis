272f2494f727d8a59bd7d3b165e47296f98a7f13
oljh <oljh@itu.dk>
2026-04-29 13:58:28 +0200

    Add Feasibility plotter

--- DotNet/CLI/PythonScripts/statistics_plotter.py
+++ DotNet/CLI/PythonScripts/statistics_plotter.py
@@ -34,9 +34,11 @@ def parse_text_file(text_file_path: str) -> dict[str, list[float]]:
 
     return header_to_data
 
+def get_figname(text_file_path) -> str:
+    return os.path.splitext(text_file_path)[0]
 
 def create_plot_from_text_file(text_file_path: str, output_folder_path):
-    figname = os.path.splitext(text_file_path)[0]
+    figname = get_figname(text_file_path)
     output_file_path = os.path.join(output_folder_path, f"{figname}.png")
     header_to_data: dict[str, list[float]] = parse_text_file(
         os.path.join(output_folder_path, text_file_path)
@@ -59,6 +61,32 @@ def create_plot_from_text_file(text_file_path: str, output_folder_path):
     print(f"Successfully saved plot to: {output_file_path}")
 
 
+def create_feasibility_plot(text_file_path: str, output_folder_path: str) -> None:
+    figname = get_figname(text_file_path)
+    output_file_path = os.path.join(output_folder_path, f"{figname}.png")
+    header_to_data: dict[str, list[float]] = parse_text_file(
+        os.path.join(output_folder_path, text_file_path)
+    )
+    
+    pop_size_header = "Population Size"
+    pop_size_points = header_to_data[pop_size_header]
+    feasible_pop_header = "Feasible Population Size"
+    feasible_pop_points = header_to_data[feasible_pop_header]
+    iterations = [x for x in range(len(pop_size_points))]
+
+    plt.plot(iterations, pop_size_points, label=pop_size_header, color='black', linestyle='--')
+    plt.plot(iterations, feasible_pop_points, label=feasible_pop_header, color='blue')
+    plt.fill_between(iterations, pop_size_points, color='blue', alpha=0.3)
+
+    plt.xlabel('Generations')
+    plt.ylabel('Population Size')
+    plt.title('MAP-Elites: Population vs. Feasibility')
+    plt.legend()
+    plt.grid(True, linestyle=':', alpha=0.6)
+
+    plt.savefig('population_growth_feasibility.png')
+
+
 def get_text_files(folder_path: str) -> list[str]:
     txt_file_paths: list[str] = []
 
@@ -78,4 +106,6 @@ if __name__ == "__main__":
     path = sys.argv[1]
     text_files = get_text_files(path)
     for text_file in text_files:
+        if "Feasibility" in text_file:
+            create_feasbility_plot(text_file, path)
         create_plot_from_text_file(text_file, path)
