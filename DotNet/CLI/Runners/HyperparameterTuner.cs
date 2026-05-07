2cfe38639ef3a8859fe89fc8c858aed002d00f1e
oljh <oljh@itu.dk>
2026-05-07 14:13:45 +0200

    Use simpler approach for hyper parameter tuner

--- DotNet/CLI/Runners/HyperparameterTuner.cs
+++ DotNet/CLI/Runners/HyperparameterTuner.cs
@@ -11,61 +11,27 @@ namespace CLI.Runners;
 public static class HyperParameterTuner
 {
     public static double FindBestSigma(
-        MapElitesArgs mapElitesArgs, 
+        MapElitesArgs mapElitesArgs,
         ConstrainedIndividualHandlerArgs constrainedIndividualHandlerArgs)
     {
-        // Range: 0.0001 to 1.0, using 16 bits of precision
-        FloatingPointChromosome chromosome = new FloatingPointChromosome(
-            [0.0001],
-            [1.0],
-            [16],
-            [4]);
+        double minSigma = 0.0;
+        double maxSigma = 1.0;
 
-        FuncFitness fitness = new FuncFitness(c =>
+        uint steps = 100;
+        double stepSize = (maxSigma - minSigma) / steps;
+
+        return Enumerable
+            .Sequence(minSigma, maxSigma, stepSize)
+            .AsParallel()
+            .Select(RunExperimentWithSigma)
+            .MaxBy(t => t.fitness).sigma;
+        
+        (double sigma, double fitness) RunExperimentWithSigma(double sigma)
         {
-            FloatingPointChromosome? floatingPointChromosome = c as FloatingPointChromosome;
-            double sigma = floatingPointChromosome.ToFloatingPoints()[0];
-
-            ConstrainedIndividualHandlerArgs newIndividualHandlerArgs = GetNewArgsWithSigma(constrainedIndividualHandlerArgs, sigma);
-            return RunMapElitesTrial(mapElitesArgs, newIndividualHandlerArgs);
-        });
-
-        int populationMin = 10;
-        int populationMax = 20;
-        int numberOfGenerations = 30;
-
-        var population = new Population(populationMin, populationMax, chromosome); 
-        var geneticAlgorithm = new GeneticAlgorithm(
-                population, 
-                fitness, 
-                new TournamentSelection(), // Less aggressive than Elite, maintains diversity
-                new UniformCrossover(0.5f), 
-                new UniformMutation())
-            {
-                Termination = new GenerationNumberTermination(numberOfGenerations),
-                TaskExecutor = new ParallelTaskExecutor() 
-            };
-        
-        geneticAlgorithm.GenerationRan += (sender, e) =>
-        {
-            var bestChromosome = geneticAlgorithm.BestChromosome as FloatingPointChromosome;
-            double bestSigma = bestChromosome.ToFloatingPoints()[0];
-            double fitness = bestChromosome.Fitness.GetValueOrDefault();
-
-            Console.WriteLine($"--- Generation {geneticAlgorithm.GenerationsNumber} ---");
-            Console.WriteLine($"Current Best Sigma: {bestSigma:F6}");
-            Console.WriteLine($"Reliability from MAP-Elites: {fitness:F2}");
-        };
-        
-        Console.WriteLine("-- Running hyper parameter tuner -- ");
-        Console.WriteLine($"Population Minimum: {populationMin}");
-        Console.WriteLine($"Population Maximum: {populationMax}");
-        Console.WriteLine($"Number of Generations: {numberOfGenerations}");
-        
-        geneticAlgorithm.Start();
-        
-        double bestSigma = (geneticAlgorithm.BestChromosome as FloatingPointChromosome).ToFloatingPoints()[0];
-        return bestSigma;
+            ConstrainedIndividualHandlerArgs newArgs = GetNewArgsWithSigma(constrainedIndividualHandlerArgs, sigma);
+            double fitness = RunMapElitesTrial(mapElitesArgs, newArgs);
+            return (sigma, fitness);
+        }
     }
 
     private static double RunMapElitesTrial(
